5697be507f961726714c726f9ade2afe11ff896e
imathno <imathno@hotmail.com>
2017-11-03 11:19:34 -0700

    Change is good

--- Team2939/src/main/java/org/xbot/ftc/operatingcode/teleop/operator_2/TeleOpGripper.java
+++ Team2939/src/main/java/org/xbot/ftc/operatingcode/teleop/operator_2/TeleOpGripper.java
@@ -1,32 +1,37 @@
 package org.xbot.ftc.operatingcode.teleop.operator_2;
 
+
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.xbot.ftc.operatingcode.teleop.XbotOperatorSubHandler;
-import org.xbot.ftc.robotcore.subsystems.arm.JewelArm;
+import org.xbot.ftc.robotcore.subsystems.cube.CubeGripper;
 
-public class TeleOpJewelSmacker extends XbotOperatorSubHandler {
+public class TeleOpGripper extends XbotOperatorSubHandler {
 
-    private JewelArm jewelArm;
+    private CubeGripper cubeGripper;
 
     @Override
     public void start(HardwareMap hardwareMap, Telemetry telemetry) {
         super.start(hardwareMap, telemetry);
-        jewelArm = (JewelArm) robotSystemsManager.getSubsystem(JewelArm.CLASS_NAME);
+        cubeGripper = (CubeGripper) robotSystemsManager.getSubsystem(CubeGripper.CLASS_NAME);
     }
 
     @Override
     public void handle(Gamepad gamepad1, Gamepad gamepad2) {
-        if (gamepad2.dpad_up) {
-            jewelArm.toggleArm();
+        if (gamepad2.right_bumper || gamepad1.right_bumper) {
+            cubeGripper.setPower(-1);
+        } else if (gamepad2.right_trigger > 0.1 || gamepad1.right_trigger > 0.1) {
+            cubeGripper.setPower(1);
+        } else {
+            cubeGripper.setPower(0);
         }
     }
 
     @Override
     public void stop() {
-        jewelArm.setPosition(0);
+        cubeGripper.setPower(0);
     }
 
     @Override
